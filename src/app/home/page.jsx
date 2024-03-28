a2a72d9de18d360a37b6eca8e91dc3e59504d13a
unknown <dejanmaric259@gmail.com>
2024-03-28 12:30:53 +0100

    fetching data from newsapi

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import Nav from '../_components/Nav/Nav'
-import Heading from '../_components/common/Heading/Heading'
-import NewsMain from '../_components/NewsMain/NewsMain'
 import HeadingNew from '../_components/HeadingNew/HeadingNew'
 import SectionHeading from '../_components/common/SectionHeading/SectionHeading'
 import NewsContainer from '../_components/NewsMain/NewsContainer'
 
 
-const getNews = async()=>{
-  const res = await fetch('https://olx.ba/'); 
-  const html = await res.text()
-  return html; 
-}
-
 export default async function Home() {
   
-    const data = await getNews() ;
-    console.log('deki dejta egete', data)
-
-
+  async function getNews(){
+    const res =await fetch(`https://newsapi.org/v2/everything?q=Apple&from=2024-03-28&sortBy=popularity&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`)
+    const data = await res.json()
+    console.log('deki', data)
+  }
+  console.log(process.env.NEXT_PUBLIC_API_KEY, 'KJEY')
+  getNews()
   return (
     <div>
       <Nav/>
