6baab90e3ce55d34d1f76e811dfc2956c7b80f7b
unknown <dejanmaric259@gmail.com>
2024-04-01 00:26:14 +0200

    fetching main heading

--- src/app/_components/HeadingNew/HeadingNew.jsx
+++ src/app/_components/HeadingNew/HeadingNew.jsx
@@ -1,17 +1,27 @@
 'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Heading from '../common/Heading/Heading'
 import styles from './HeadingNew.module.scss';
 
 export default function HeadingNew() {
+  const [headingNew,setHeadingNew]=useState([])
+
+  useEffect(()=>{
+    async function getHeadingNew(){
+      const res =await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apikey=${process.env.NEXT_PUBLIC_API_KEY}`)
+      const data = await res.json()
+      setHeadingNew(data.articles[0])
+    }
+    getHeadingNew()
+  },[])
   return (
     <div className={styles.mainContainer}>
         <Image width={500} height={320} src='https://images.unsplash.com/photo-1564148204877-9bf116988675?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg' className={styles.mainImg}/>
         <div className={styles.container}>
-            <span className={styles.date}>1 day ago</span>
-            <Heading text='Scientists Uncover Ancient Fossilized Forest in Antarctica'/>
-            <p className={styles.paragraph}>In a groundbreaking discovery, a team of international scientists has unearthed the remains of an ancient forest buried beneath Antarctica's icy landscape. The findings, published in the prestigious journal Nature, shed light on the continent's distant past and provide valuable insights into its geological history.</p>
+            <span className={styles.date}>{headingNew.publishedAt}</span>
+            <Heading text={headingNew.title}/>
+            <p className={styles.paragraph}>{headingNew.description}</p>
         </div>
     </div>
   )
