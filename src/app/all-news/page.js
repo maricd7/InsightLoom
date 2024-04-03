04ac781f2e089b07be7ad22ed54a3dee6e84bc53
unknown <dejanmaric259@gmail.com>
2024-04-03 19:33:48 +0200

    nav on all news page

--- src/app/all-news/page.js
+++ src/app/all-news/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AllArticles from '../_components/AllArticles/AllArticles'
+import Nav from '../_components/Nav/Nav'
 
 function allNews() {
   return (
     <div>
+        <Nav></Nav>
         <AllArticles/> 
     </div>
   )
